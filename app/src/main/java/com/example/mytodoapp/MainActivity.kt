258aa220b0f7abe29d08b0b144f46b17ff7f6dd3
7yes <100316346+7yes@users.noreply.github.com>
2023-09-07 12:29:36 -0500

    dependencies

--- app/src/main/java/com/example/mytodoapp/MainActivity.kt
+++ app/src/main/java/com/example/mytodoapp/MainActivity.kt
@@ -3,16 +3,20 @@ package com.example.mytodoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.mytodoapp.addtasks.ui.TaskViewModel
+import com.example.mytodoapp.addtasks.ui.TasksScreen
 import com.example.mytodoapp.ui.theme.MyTodoAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val taskViewModel:TaskViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,25 +26,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TasksScreen(taskViewModel)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyTodoAppTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/example/mytodoapp/TodoApp.kt
@@ -0,0 +1,9 @@
+package com.example.mytodoapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TodoApp:Application() {
+    //
+}
